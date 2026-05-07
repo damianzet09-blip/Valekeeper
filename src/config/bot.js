3b8025c0c0c3c1d658757a04075e0920ccb0a9de
damianzet09-blip <damianzet09@o2.pl>
2026-05-07 19:24:16 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Grinding PvP Score",
+        name: "Overseeing Darkvale",
         // Activity type number (0 = Playing).
-        type: 1, 
+        type: 4, 
       },
     ],
   },
